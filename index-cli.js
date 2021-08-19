b8578568404de1a6018553b0d5b40655524855ed
Guillermo de la Iglesia <zguillez@gmail.com>
2021-08-19 17:44:18 +0200

    update

--- index-cli.js
+++ index-cli.js
@@ -1,7 +1,7 @@
 #! /usr/local/bin/node
 const zversion = require('./index');
 const argv = require('minimist')(process.argv.slice(2));
-if (argv.v || argv.version) {
+if (argv.u || argv.update) {
   if (argv.major) {
     zversion.update('major');
   } else if (argv.minor) {
